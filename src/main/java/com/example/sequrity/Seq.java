63d97861d23e02939911477efc806c396b70dd1e
Трушенков Дмитрий <diman28051999@gmail.com>
2018-12-19 14:58:24 +0300

    removed few annotations

--- src/main/java/com/example/sequrity/Seq.java
+++ src/main/java/com/example/sequrity/Seq.java
@@ -1,25 +1,18 @@
 package com.example.sequrity;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @PropertySource("classpath:application.properties")
-@EnableAutoConfiguration
-@Configuration
-@ComponentScan
-@EnableJdbcHttpSession
+@SpringBootApplication
 public class Seq {
 
     public static void main(String[] args) {
         SpringApplication.run(Seq.class, args);
     }
-
 
 }
 
